a78fddcb01e6b252ce875aa3c5bd9479700ae4dd
telida <sprv.adilet@gmail.com>
2025-10-10 01:59:06 +0500

    configured logger

--- Motion2Gif/MainWindow.axaml.cs
+++ Motion2Gif/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using Serilog;
 
 namespace Motion2Gif;
 
@@ -7,5 +13,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void ChooseFile_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var topLevel = GetTopLevel(this);
+
+        if (topLevel == null)
+            throw new InvalidOperationException("Top level is null");
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open a Video File",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            var file = files[0];
+            var fileProperties = await file.GetBasicPropertiesAsync();
+            Log.Information($"""
+                            
+                            File name: {file.Name}
+                            File path: {file.Path}
+                            Date Created: {fileProperties.DateCreated}
+                            Date Modified: {fileProperties.DateModified}
+                            Size: {fileProperties.Size} bytes
+                            """
+            );
+        }
     }
 }
