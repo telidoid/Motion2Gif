08208ae389d4147c48d6bcb0f77b43229bd040b7
telida <sprv.adilet@gmail.com>
2025-10-19 13:28:46 +0500

    logs: sink to file

--- Motion2Gif/Program.cs
+++ Motion2Gif/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using Avalonia.Logging;
 using Serilog;
+using LogEventLevel = Serilog.Events.LogEventLevel;
 
 namespace Motion2Gif;
 
@@ -13,7 +14,7 @@ internal static class Program
     [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
-    
+
     // Avalonia configuration, don't remove; also used by visual designer.
     private static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
@@ -21,13 +22,20 @@ internal static class Program
             .WithInterFont()
             .ConfigureAppLogging()
             .LogToTrace();
-    
+
     private static AppBuilder ConfigureAppLogging(this AppBuilder appBuilder)
     {
         Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
+            .WriteTo.File(
+                path: "Logs/log-.txt",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 30,
+                rollOnFileSizeLimit: true,
+                restrictedToMinimumLevel: LogEventLevel.Information
+            )
             .CreateLogger();
-        
+
         return appBuilder;
     }
 }
